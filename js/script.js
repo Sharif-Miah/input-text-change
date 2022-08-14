1d48220785e30406fb61967e6470d6ebaa692f9e
farhan sharif <farhansharif1410@gmail.com>
2022-08-14 00:02:16 -0230

    starting work

---
+++ js/script.js
@@ -0,0 +1,7 @@
+
+document.getElementById('input-btn').addEventListener('click', function () {
+    const inputField = document.getElementById('input-field');
+
+    const inputText = inputField.value;
+
+});
